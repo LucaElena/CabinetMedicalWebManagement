bda6d365663a8a2fff8689b51d897e64f4a15a18
Luca Elena <36001301+LucaElena@users.noreply.github.com>
2021-05-22 21:29:38 +0300

    mvc work

--- MvcPart/app/controllers/home.php
+++ MvcPart/app/controllers/home.php
@@ -3,48 +3,50 @@
 	class Home extends Controller
 	{
 
-		public function index($user_id = "")
+		public function index($userId = "")
 		{
 
-            //TO DO: 
-            //aici ar trebuii sa avem 
-            //          daca nu suntem logati o pagina cu select login(as cabinet/doctor/patient)
-            //          daca suntem logati -> verficam ce timp de user avem incarcat -> redirect la pagina lui home (cabinet/index sau doctor/index sau patient/index) 
-            // temporar punem un cabine din db :
-            $user_id = "cabinet_ana";
+                  //TO DO: 
+                  //aici ar trebuii sa avem 
+                  //          daca nu suntem logati o pagina cu select login(as cabinet/doctor/patient)
+                  //          daca suntem logati -> verficam ce timp de user avem incarcat -> redirect la pagina lui home (cabinet/index sau doctor/index sau patient/index) 
+                  // temporar punem un cabine din db :
+                  // $userId = "cabinet_ana";
 
-            $user = $this->model('UserModel');
-
-            if($user_id)
-            {
-                  $user_exist = $user->isDefined($userId);
-                  if ($user_exist)
+                  $user = $this->model('UserModel');
+                  print_r("Home index ", $userId);
+                  if($userId)
                   {
-                        $user_type = $user->getUserType($userId);
-                        switch ($user_type) 
+                        $user_exist = $user->isDefined($userId);
+                        if ($user_exist)
                         {
-                              case "cabinet":
-                                    $this->view('cabinet/' . $userId,  ['id' => $userId]);
-                                    break;
-                              case "doctor":
-                                    $this->view('doctor/' . $userId,  ['id' => $userId]);
-                                    break;
-                              case "patient":
-                                    $this->view('patient/' . $userId,  ['id' => $userId]);
-                                    break;
-                              }
+                              $user_type = $user->getUserType($userId);
+                              switch ($user_type) 
+                              {
+                                    case "cabinet":
+                                          $this->view('cabinet/index' ,  ['id' => $userId]);
+                                          break;
+                                    case "doctor":
+                                          $this->view('doctor/index' ,  ['id' => $userId]);
+                                          break;
+                                    case "patient":
+                                          $this->view('patient/index' ,  ['id' => $userId]);
+                                          break;
+                                    }
+                        }
+                        else
+                        {
+                              $this->view('errors/404',  ['id' => $userId]);
+                        }
                   }
                   else
                   {
-                        $this->view('errors/404',  ['id' => $userId]);
+                        $this->view('errors/403',  ['id' => $userId]);
                   }
             }
-            else
-            {
-                $this->view('errors/403',  ['id' => $userId]);
-            }
-		
+      }
 
-	}
+
+	
 
 ?>
--- MvcPart/app/models/usermodel.php
+++ MvcPart/app/models/usermodel.php
@@ -3,26 +3,84 @@
 class UserModel extends Controller
 {
     public $id;
-    //SELECT * from users;
+    public $type;
+    
+    // check if user exist in db
+    public function isDefined($user_name)
+    {
+        $sql = "SELECT COUNT(user_name) FROM users where user_name = :username";
+        $query = $this->conn->prepare($sql);
+        $query->execute(array(":username" => $user_name));
+        $results = $query->fetch(PDO::FETCH_ASSOC);
+
+        // print_r("<br>Suntem in UserModel isDefined and results:");
+        // print_r($results);
+        
+        if ($results['COUNT(user_name)'] > 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    // return general data based on user id
     public function getData($username)
     {
         $sql = "SELECT * from users WHERE user_name = :username";
         $query = $this->conn->prepare($sql);
         $query->execute(array(":username" => $username));
         $results = $query->fetch(PDO::FETCH_ASSOC);
-        print_r("Suntem in UserModel getData and results:");
-        print_r($results);
+        // print_r("<br>Suntem in UserModel getData and results:");
+        // print_r($results);
         return $results;
     }
+
+    public function checkType($type , $idUser)
+    {
+        $sql_isType = "SELECT COUNT(id_user) FROM " . $type . " WHERE id_user = " . $idUser;
+        // print_r("<br>checkType  " . $type);
+    
+        $query_isType = $this->conn->prepare($sql_isType);
+        $query_isType->execute();
+        $result = $query_isType->fetch(PDO::FETCH_ASSOC);
+        $typeCount = $result['COUNT(id_user)'];
+        // print_r($typeCount);
+        if($typeCount > 0)
+            return true;
+        return false;
+    }
+
+    // Based on a user id we return the type (cabinet/doctor/patient)
     public function getUserType($username)
     {
-        $sql = "SELECT * from users WHERE user_name = :username";
-        $query = $this->conn->prepare($sql);
-        $query->execute(array(":username" => $username));
-        $results = $query->fetch(PDO::FETCH_ASSOC);
-        print_r("Suntem in UserModel getData and results:");
-        print_r($results);
-        return $results;
+        $sql_getId= "SELECT id_user FROM users WHERE user_name = :username";
+        $query_getId = $this->conn->prepare($sql_getId);
+        $query_getId->execute(array(":username" => $username));
+        $result = $query_getId->fetch(PDO::FETCH_ASSOC);
+        $id_user = $result['id_user'];
+        $this->id =  $id_user;
+        if($id_user)
+        {
+            if($this->checkType("cabinets" , $id_user))
+            {
+                $this->type = "cabinet";
+                return "cabinet";
+            }
+            if($this->checkType("doctors" , $id_user))
+            {
+                $this->type = "doctor";
+                return "doctor";
+            }
+            if($this->checkType("patients" , $id_user))
+            {
+                $this->type = "patient";
+                return "patient";
+            }
+        }
+        return "";
     }
     
 }
